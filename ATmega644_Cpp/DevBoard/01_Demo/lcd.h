037ac949663c9788ce5601b0effa1933b80ce74f
Beszti85 <csababesztercei@gmail.com>
2020-04-19 06:48:51 +0200

    Added lcd.h

---
+++ ATmega644_Cpp/DevBoard/01_Demo/lcd.h
@@ -0,0 +1,21 @@
+/*
+ * lcd.h
+ *
+ * Created: 2020. 03. 10. 22:37:23
+ *  Author: drCsabesz
+ */ 
+
+
+#ifndef LCD_H_
+#define LCD_H_
+
+
+class CharLcd
+{
+private:
+protected:
+public:
+};
+
+
+#endif /* LCD_H_ */
